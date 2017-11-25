29d016634a14f92909c01c2150531672d216585a
Michael Göhler <github@nixhub.de>
2017-11-25 17:48:47 +0100

    watch mon-fri 19-21 only

--- lambda/beach38_monitor.py
+++ lambda/beach38_monitor.py
@@ -18,7 +18,10 @@ beach38_url = 'https://ssl.forumedia.eu/beach38-courtbuchung.de/reservations.php
 forecast = 7
 
 # watched time ranges (string)
-watched_ranges = ['17:00 - 19:00 Uhr', '19:00 - 21:00 Uhr']
+watched_timeslots = ['19:00 - 21:00 Uhr']
+
+# watched week days (string)
+watched_weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
 
 # let the magic begin
 
@@ -34,73 +37,75 @@ def lambda_handler(event, context):
     notifications = []
     for i in range(forecast):
         date = datetime.now().replace(hour=23, minute=59, second=59, microsecond=0) + timedelta(days=i+1)
-        ttl = decimal.Decimal(date.strftime('%s'))
 
-        for court_type in range(2):
-            response = requests.get(beach38_url.format(court_type + 1, date.strftime('%Y-%m-%d')))
-            html = BeautifulSoup(response.text)
+        if date.strftime('%a') in watched_weekdays:
+            ttl = decimal.Decimal(date.strftime('%s'))
 
-            for table in html.body.findAll('table', attrs={'class': 'areaPeriods'}):
-                court_name = table.find('th').text.encode('ascii', errors='ignore')
+            for court_type in range(2):
+                response = requests.get(beach38_url.format(court_type + 1, date.strftime('%Y-%m-%d')))
+                html = BeautifulSoup(response.text)
 
-                for row in table.findAll('tr'):
-                    cell = row.find('td')
+                for table in html.body.findAll('table', attrs={'class': 'areaPeriods'}):
+                    court_name = table.find('th').text.encode('ascii', errors='ignore')
 
-                    if cell:
-                        current_range = cell.text.encode('ascii', errors='ignore')
+                    for row in table.findAll('tr'):
+                        cell = row.find('td')
 
-                        if current_range in watched_ranges:
-                            court_status = dict(cell.attrs).get('class', '')
+                        if cell:
+                            current_timeslot = cell.text.encode('ascii', errors='ignore')
 
-                            # see if we already stored this combination of court_name and reservation_time
-                            item = dynamotable.get_item(Key={
-                                'court_name': court_name,
-                                'reservation_time': '{} {}'.format(date.strftime('%d.%m.%Y'), current_range)
-                            })
+                            if current_timeslot in watched_timeslots:
+                                court_state = dict(cell.attrs).get('class', '')
 
-                            # build the notification string
-                            notification = '{} {} on {} {}'.format(
-                                court_name,
-                                court_status,
-                                date.strftime('%a %d.%m.%Y'),
-                                current_range
-                            )
+                                # see if we already stored this combination of court_name and reservation_time
+                                item = dynamotable.get_item(Key={
+                                    'court_name': court_name,
+                                    'reservation_time': '{} {}'.format(date.strftime('%d.%m.%Y'), current_timeslot)
+                                })
 
-                            if court_status == 'avaliable':
-                                if 'Item' not in item or item['Item']['court_state'] != 'avaliable':
-                                    # either the court reservation was never stored and is available,
-                                    # or the courts status changed to available
+                                # build the notification string
+                                notification = '{} {} on {} {}'.format(
+                                    court_name,
+                                    court_state,
+                                    date.strftime('%a, %d.%m.%Y'),
+                                    current_timeslot
+                                )
+
+                                if court_state == 'avaliable':
+                                    if 'Item' not in item or item['Item']['court_state'] != 'avaliable':
+                                        # either the court reservation was never stored and is available,
+                                        # or the courts status changed to available
+                                        notifications.append(notification)
+
+                                if 'Item' in item and item['Item']['court_state'] == 'avaliable' and court_state != 'avaliable':
+                                    # or the courts status changed from available to something else
                                     notifications.append(notification)
 
-                            if 'Item' in item and item['Item']['court_state'] == 'avaliable' and court_status != 'avaliable':
-                                # or the courts status changed from available to something else
-                                notifications.append(notification)
+                                if 'Item' in item:
+                                    if item['Item']['court_state'] != court_state:
+                                        # if the status changed update it in our dynamodb table
+                                        print "update:", date.strftime('%d.%m.%Y'), current_timeslot, court_name, court_state
+                                        dynamotable.update_item(
+                                            Key={
+                                                'court_name': court_name,
+                                                'reservation_time': '{} {}'.format(date.strftime('%d.%m.%Y'), current_timeslot)
+                                            },
+                                            UpdateExpression="set court_state = :s",
+                                            ExpressionAttributeValues={
+                                                ':s': court_state
+                                            },
+                                            ReturnValues="UPDATED_NEW"
+                                        )
 
-                            if 'Item' in item:
-                                if item['Item']['court_state'] != court_status:
-                                    # if the status changed update it in our dynamodb table
-                                    print "update:", date.strftime('%d.%m.%Y'), current_range, court_name, court_status
-                                    dynamotable.update_item(
-                                        Key={
-                                            'court_name': court_name,
-                                            'reservation_time': '{} {}'.format(date.strftime('%d.%m.%Y'), current_range)
-                                        },
-                                        UpdateExpression="set court_state = :s",
-                                        ExpressionAttributeValues={
-                                            ':s': court_status
-                                        },
-                                        ReturnValues="UPDATED_NEW"
-                                    )
-
-                            else:
-                                # if it doesn't exist already, insert the court reservation into our dynamodb table
-                                print "insert:", court_name, date.strftime('%d.%m.%Y'), current_range, court_status
-                                dynamotable.put_item(Item={
-                                    'court_name': court_name,
-                                    'reservation_time': '{} {}'.format(date.strftime('%d.%m.%Y'), current_range),
-                                    'court_state': court_status,
-                                    'retention': ttl
-                                })
+                                else:
+                                    # if it doesn't exist already, insert the court reservation into our dynamodb table
+                                    print "insert:", court_name, date.strftime('%d.%m.%Y'), current_timeslot, court_state
+                                    dynamotable.put_item(Item={
+                                        'court_name': court_name,
+                                        'reservation_time': '{} {}'.format(date.strftime('%d.%m.%Y'), current_timeslot),
+                                        'court_state': court_state,
+                                        'retention': ttl
+                                    })
 
     print 'notifications:', notifications
 
